af23ccb64649fd92d690bd899dd072965819c779
imarevic <i.marevic@gmx.de>
2020-04-01 23:52:40 +0200

    data processing tests

--- logic/processing.py
+++ logic/processing.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 # data processing
 def process_data(inf_dict, friends_dict, profile_dict, lk_dict):
-    print(inf_dict, '/n/n')
-    print(friends_dict, '/n/n')
-    print(profile_dict, '/n/n')
-    print(lk_dict, '/n/n')
+
+    lk_df = pd.DataFrame(profile_dict)
+    print(lk_df.head)
--- scraper.py
+++ scraper.py
@@ -10,13 +10,13 @@ def run_scraper():
     main entry point of the program.
     """
     # global driver object
-    driver = sc.create_driver("Chrome")
+    #driver = sc.create_driver("Chrome")
     # login to page
-    driver = sc.login(driver)
+    #driver = sc.login(driver)
     # scrape content of pages with relevant data
-    driver, d_inf, d_friends = sc.scrape_home_page(driver)
-    driver, d_profile = sc.scrape_profile_page(driver)
-    driver, d_lk = sc.scrape_lk_page(driver)
+    #driver, d_inf, d_friends = sc.scrape_home_page(driver)
+    #driver, d_profile = sc.scrape_profile_page(driver)
+    #driver, d_lk = sc.scrape_lk_page(driver)
     # post process_data
     d_inf, d_friends, d_profile, d_lk = t.load_dicts('d_inf_dict',
                                                     'd_friends_dict',
