dc8068f90465032bf49e590bb23ec0c09fbe698f
imarevic <i.marevic@gmx.de>
2020-04-13 15:34:50 +0200

    split wins and losses logic

--- scraping/logic/processing.py
+++ scraping/logic/processing.py
@@ -14,5 +14,29 @@ def process_data(inf_dict, friends_dict, profile_dict, lk_dict):
     # rename columns in profile df
     profile_df.columns = profile_col_names
     # calculate additional infos and clean up dfs
+    inf_df['info_rank'] = inf_df['info_rank'].str.split()
+    inf_df['info_rank'] = inf_df['info_rank'].values.tolist()[0][1]
 
+    profile_df = split_wins_and_losses(profile_df, ':')
+    print(profile_df)
     # return json objects
+
+
+def split_wins_and_losses(df, separator):
+    """
+    splits a column by given separator and
+    creates new columns
+    args:
+    df: a data frame
+    separator: separator to split by
+    returns: new df wit split columns
+    """
+
+    for col in df:
+        lst_entry = df[col].str.split(separator)[0]
+        len_lst_entry = len(lst_entry)
+        if len_lst_entry > 1:
+            df[col + '_win'] = lst_entry[0]
+            df[col + '_loss'] = lst_entry[1]
+            del df[col]
+    return df
